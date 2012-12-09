9c0beab9ccdffb032e7b1a2ad575ad7f361471e0
山本　安輝 <yasuteru_yamamoto@fandmnet.com>
2012-12-09 16:11:12 +0900

    コメント追加など

--- HelloWorld/main.m
+++ HelloWorld/main.m
@@ -6,13 +6,17 @@
 //  Copyright (c) 2012年 Yamamoto Yasuteru. All rights reserved.
 //
 
+//  Frameworks から UI 用のクラスをインポート
 #import <UIKit/UIKit.h>
 
+//  最初に起動するアプリケーションクラスをインポート
 #import "HelloWorldAppDelegate.h"
 
+//  main から実行される
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
+        //  インポートした HelloWorldAppDelegate を実行
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([HelloWorldAppDelegate class]));
     }
 }
--- README.md
+++ README.md
@@ -4,3 +4,19 @@ HelloWorld
 ## 初めてのiOSアプリ ケーション ##
 https://developer.apple.com/jp/devcenter/ios/library/documentation/iPhone101.pdf
 
+# 実行順
+
+まず最初に、
+`/HelloWorld/Supporting Files/main.m`
+が処理される。
+delegate をロードする。
+
+ロードされる delegate
+`/HelloWorld/HelloWorldAppDelegate.[h|m]`
+
+その後、
+`/HelloWorld/Supporting Files/HelloWorkd-Info.plist`
+で設定されている、
+`Main storybord file base name`
+の storybord がロードされる
+
